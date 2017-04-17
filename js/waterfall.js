2e81f8e290a998c8a7d4abb0b7e8a9b68110cb1d
Bothdream <1368463814@qq.com>
2017-04-17 22:40:13 +0800

    瀑布流无限加载

--- js/waterfall.js
+++ js/waterfall.js
@@ -1,12 +1,14 @@
 ;(function(){
    /******瀑布流对象*****/
    var Waterfall = function(config){
+       var self = this;
+       this.flag = true;
        //默认配置的参数
        this.config = {
        	  "selectId":"wf-waterfall",//瀑布流的id选择器
        	  "width":200,//宽度
-       	  "column":5,//列数
-       	  "speed":5000//动画的速度
+       	  "column":4,//列数
+       	  "speed":3000//动画的速度
        };
        //手动配置
        if(config){
@@ -15,6 +17,41 @@
        //设置默认参数
        this.setConfig();
        this.waterfalls();
+       //绑定滚动事件
+       $(window).scroll(function(){
+            var sh = document.documentElement.scrollHeight;
+            var ch = document.documentElement.clientHeight;
+            var cw = document.documentElement.clientWidth;
+
+            if(self.flag){
+                  self.flag = false;
+                  for (var i = 0; i < 6; i++) {
+                    
+                  
+                  var nwf = self.wfitems.eq(0).clone().appendTo(self.waterfall).css({
+                        width:self.config.width,
+                        position:"absolute",
+                        top:self.createRandomNumber(0,ch/2),
+                        left:self.createRandomNumber(0,cw/2),
+                        opacity:0 
+                  });
+                  var obj = self.minHightToIndex(self.arr);
+                    //重新更新this.arr中的值
+                  self.arr[obj.index] += nwf.outerHeight();
+                  var w = nwf.outerWidth();
+                  nwf.animate({
+                        position:"absolute",
+                        top:obj.height,
+                        left:obj.index*w,
+                        opacity:1
+                  },self.config.speed,function(){
+                       self.flag = true;
+                  });
+                }
+            }
+
+
+       });
    };
    /*****原型继承*****/
    Waterfall.prototype = {
@@ -29,12 +66,13 @@
            this.waterfall.css({width:wfwidth});
            //获取wf-waterfall下的所有wf-item
            this.wfitems = this.waterfall.children();
-           //设置所有wf-item的宽度
+           //获得客户区的宽度和高度
            var cw = document.documentElement.clientWidth;
            var ch = document.documentElement.clientHeight;
+           //设置所有wf-item的宽度
            this.wfitems.each(function(){
            	    $(this).css({
-           	    	width:_this_.config.width,
+           	    	  width:_this_.config.width,
                     position:"absolute",
                     top:_this_.createRandomNumber(0,ch),
                     left:_this_.createRandomNumber(0,cw),
@@ -58,7 +96,7 @@
          * { function_description }
          */
         waterfalls:function(){
-        	//将this.wfitems转换为数组
+        	  //将this.wfitems转换为数组
             for (var i = this.config.column; i < this.wfitems.size();i++) {
             	//获取数组中的最小高度和索引值
             	var obj = this.minHightToIndex(this.arr);
@@ -95,11 +133,13 @@
             }
         },
         createRandomNumber:function(min,max){
-        	var m = max > min?min:max;
-        	var diff = max - min > 0? max -min:min - max;
-        	return m + diff*Math.random();
+        	   var m = max > min?min:max;
+        	   var diff = max - min > 0? max -min:min - max;
+        	   return m + diff*Math.random();
+        },
+        scrollUpdate:function(){
+             
         } 
-        
    };
    /****将瀑布流对象绑定在window对象***/
    window['waterfall'] = Waterfall;
